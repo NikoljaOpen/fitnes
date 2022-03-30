61ba91827df4032c1bcabd5952dc41ad24f60de9
Nikolai Mikhailov <nikoljahromoj@gmail.com>
2022-03-30 09:33:59 +0300

    первый коммит

---
+++ AutorizationLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutorizationLibrary
+{
+    public static class AutorizationClass
+    {
+        public static bool CheckValues(string login,string password)
+        {
+            if (login == "admin" && password == "admin")
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Fitnes/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Fitnes.ClientWindows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutorizationLibrary;
+
+namespace Fitnes
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var password = PasswordBox.Password;
+            if(AutorizationClass.CheckValues(login,password))
+            {
+                RootWindow window = new RootWindow();
+                window.Show();
+                this.Close();
+            }
+            
+        }
+    }
+}
